a266157a31053e0eb35b9affc4a0fd9b816fa3ad
Lukasz Drzewiecki <lukasz.j.drzewiecki@gmail.com>
2024-11-13 00:09:15 +0100

    Zamek BFS

--- zam/zam.py
+++ zam/zam.py
@@ -1,34 +1,56 @@
+from collections import deque
 import sys
 
 sys.set_int_max_str_digits(1_000_000_000)
 
 
-def minimal_button_presses(n):
-    queue = [(n, 0)]
-    visited_set = {n}
-
-    index = 0
-    while index < len(queue):
-
-        current, presses = queue[index]
-        index += 1
-
-        if current == 1:
-            return presses
-
-        next_num = current + 1
-        if next_num not in visited_set:
-            visited_set.add(next_num)
-            queue.append((next_num, presses + 1))
-
-        current_str = str(current)
-        if len(current_str) > 1:
-            rotated_num = int(current_str[1:] + current_str[0])
-            if rotated_num not in visited_set:
-                visited_set.add(rotated_num)
-                queue.append((rotated_num, presses + 1))
+def rotate(number):
+    """
+    Funkcja obraca liczbę cyklicznie. Przenosi najbardziej znaczącą cyfrę
+    na koniec i usuwa zera wiodące w wynikowej liczbie.
+    """
+    number_str = str(number)
+    rotated_number = int(number_str[1:] + number_str[0])
+    return rotated_number
 
 
-n = int(input().strip())
+def minimum_presses_to_one(n):
+    """
+    Funkcja oblicza minimalną liczbę naciśnięć przycisków, aby uzyskać liczbę 1.
+    """
+    if n == 1:
+        return 0  # Jeśli początkowa liczba to 1, nie musimy wykonywać żadnych operacji
 
-print(minimal_button_presses(n))
+    queue = deque([(n, 0)])  # Kolejka z parą (liczba, liczba kroków)
+    visited = set([n])  # Zbiór odwiedzonych stanów, aby uniknąć zapętlenia
+
+    while queue:
+        current_number, steps = queue.popleft()
+
+        # Operacja: Dodaj 1 do liczby
+        incremented_number = current_number + 1
+        if incremented_number == 1:
+            return steps + 1
+        if incremented_number not in visited:
+            visited.add(incremented_number)
+            queue.append((incremented_number, steps + 1))
+
+        # Operacja: Cyfrowy obrót liczby
+        rotated_number = rotate(current_number)
+        if rotated_number == 1:
+            return steps + 1
+        if rotated_number not in visited:
+            visited.add(rotated_number)
+            queue.append((rotated_number, steps + 1))
+
+    return -1  # W przypadku gdyby liczba 1 nie była osiągalna, choć teoretycznie jest
+
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    print(minimum_presses_to_one(n))
+
+    # start = time.time()
+    # print(minimal_button_presses_new(n))
+    # print(minimal_button_presses(n))
+    # print(time.time()-start)
